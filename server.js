8ad53d78ac0f531cff17e24a9287d13a293c1fdf
ageekdad <cloud9-c9.io@ageekdad.net>
2016-12-12 22:21:06 +0000

    Basic functionality

--- server.js
+++ server.js
@@ -4,7 +4,29 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!!')
+    
+    var d = req.query.date;
+    
+    if(!d) {
+        res.send(`{date: null, timestamp: null}`);
+        return;
+    }
+    
+    var timestamp = parseInt(d);
+    var date = d;
+    if(timestamp) {
+        date = new Date(timestamp);
+    }
+    else {
+        date = new Date(d);
+        timestamp = date.getTime();
+        if(!timestamp) {
+            res.send(`{date: null, timestamp: null}`);
+            return;
+        }
+    }
+    
+    res.send(`{ date: "` + date + `", timestamp: ` + timestamp + ` }`);
 })
 
 app.listen(8080, function () {
